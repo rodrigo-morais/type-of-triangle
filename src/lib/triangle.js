82de21324bdb1e4e22223fa656ac089d4847aae6
rodrigo-morais <morais.rm@gmail.com>
2018-12-09 11:00:08 +0100

    Adding library to evaluate triangles.

---
+++ src/lib/triangle.js
@@ -0,0 +1,15 @@
+const sumTwoSmallest = (a, b, c) => a + b + c - Math.max(...[a, b, c])
+
+const largest = (a, b, c) => Math.max(...[a, b, c])
+
+const isValid = (a, b, c) => a > 0 && b > 0 && c > 0 && sumTwoSmallest(a, b, c) >= largest(a, b, c)
+
+const isEquilateral = (a, b, c) => a === b && b === c
+
+const isIsosceles = (a, b, c) => a === b || a === c || b === c
+
+const getTriangleType = (a, b, c) => isValid(a, b, c) ?
+  isEquilateral(a, b, c) && 'EQUILATERAL' || isIsosceles(a, b, c) && 'ISOSCELES' || 'SCALENE' :
+  'INVALID'
+
+export default getTriangleType
---
+++ tests/lib/triangle.test.js
@@ -0,0 +1,39 @@
+import getTringleType from '../../src/lib/triangle'
+
+describe('#getTriangleType', () => {
+  describe('when sides are 0', () => {
+    it('returns INVALID', () => {
+      expect(getTringleType(0, 0, 0)).toEqual('INVALID')
+    })
+  })
+
+  describe('when at least one side is 0', () => {
+    it('returns INVALID', () => {
+      expect(getTringleType(4, 0, 3)).toEqual('INVALID')
+    })
+  })
+
+  describe('when one side is bigger than the sum of the other two sides', () => {
+    it('returns INVALID', () => {
+      expect(getTringleType(4, 10, 3)).toEqual('INVALID')
+    })
+  })
+
+  describe('when all sides are equal', () => {
+    it('returns an EQUILATERAL triangle', () => {
+      expect(getTringleType(3, 3, 3)).toEqual('EQUILATERAL')
+    })
+  })
+
+  describe('when two sides are equal', () => {
+    it('returns an ISOSCELES triangle', () => {
+      expect(getTringleType(3, 4, 3)).toEqual('ISOSCELES')
+    })
+  })
+
+  describe('when all sides are different', () => {
+    it('returns an SCALENE triangle', () => {
+      expect(getTringleType(3, 6, 4)).toEqual('SCALENE')
+    })
+  })
+})
